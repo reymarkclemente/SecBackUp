27fd40956e1f035952317d1b2b0551edc0e013f0
reymarkclemente <reymarkclemente@gmail.com>
2025-09-20 12:33:02 +0800

    Add files via upload

---
+++ TASK1 SECURING CORE SWITCH.ps1
@@ -0,0 +1,22 @@
+﻿windows for SOC:
+cmd
+cd deskstop
+mkdir _NAMEMO
+
+task1: SECURING CORE SWITCH:
+configure terminal
+username admin privilege 15 secret pass
+username mac privilege 15 secret pass
+line vty 0 14
+ login local 
+ transport input all
+ end
+
+ task2: why companies require vpn for wfh/remote work
+ -forticlient, cisco anywhere, openvpv
+ implement security on switch:
+ config t
+ ip domain-name rivanit.com
+ crypto key generate rsa
+ ip ssh version 2
+ end
